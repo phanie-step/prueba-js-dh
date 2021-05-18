26045bcd5858ad9fe7a26caf7ab846405215e890
paco <paco@MacBook-Pro-de-pako.local>
2021-05-17 19:12:51 -0500

    test working

--- app.js
+++ app.js
@@ -11,18 +11,12 @@ exports.leerTareas = function () {
     /*
         En esta función debes leer y retornar las tareas registradas.
     */
-    const tareas = require('./tareas.json');
-    return tareas;
 }
 
 exports.agregarTarea = function (tarea) {
     /*
         Registra y guarda una nueva tarea.
     */
-    const tareas = require('./tareas.json');
-    tareas.push(tarea);
-    const {writeFileSync} = require('fs');
-    writeFileSync('./tareas.json',JSON.stringify(tareas));
 }
 
 exports.filtrarTareasPorEstado = function (estado) {
@@ -30,6 +24,4 @@ exports.filtrarTareasPorEstado = function (estado) {
         En esta función debes de leer las tareas y retornar las que tengan el estado que se
         manda en el parametro.
     */
-    const tareas = require('./tareas.json');
-    return tareas.filter(tarea => tarea.status === estado);
 }
