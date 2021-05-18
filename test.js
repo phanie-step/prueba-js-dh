d73ce16f660f1b5560322cbf4ae38eabef469def
paco <paco@MacBook-Pro-de-pako.local>
2021-05-17 19:20:52 -0500

    color test

--- test.js
+++ test.js
@@ -17,24 +17,22 @@ const { writeFileSync } = require('fs');
     agregarTarea(tempData[1]);
     const respuesta = leerTareas();
     if (!respuesta.length || respuesta.length !== 2) {
-        console.error(`error: leyendo o escribiendo tareas, esperando: ${JSON.stringify(tempData)}, resultado: ${JSON.stringify(respuesta)}`);
+        console.error("\x1b[31m", `error: leyendo o escribiendo tareas, esperando: ${JSON.stringify(tempData)}, resultado: ${JSON.stringify(respuesta)}`);
         return;
     }
 
     const respuestaFiltrado1 = filtrarTareasPorEstado('pendiente');
     const respuestaFiltrado2 = filtrarTareasPorEstado('terminada');
 
-    console.log({respuestaFiltrado1, respuestaFiltrado2})
-
     if (respuestaFiltrado1[0].titulo !== tempData[0].titulo) {
-        console.error(`error obteniendo tareas filtradas por estado, esperando ${JSON.stringify([tempData[0]])}, resultado: ${JSON.stringify(respuestaFiltrado1)}`);
+        console.error("\x1b[31m", `error obteniendo tareas filtradas por estado, esperando ${JSON.stringify([tempData[0]])}, resultado: ${JSON.stringify(respuestaFiltrado1)}`);
         return;
     }
 
     if (respuestaFiltrado2[0].titulo !== tempData[1].titulo) {
-        console.error(`error obteniendo tareas filtradas por estado, esperando ${JSON.stringify([tempData[1]])}, resultado: ${JSON.stringify(respuestaFiltrado2)}`);
+        console.error("\x1b[31m", `error obteniendo tareas filtradas por estado, esperando ${JSON.stringify([tempData[1]])}, resultado: ${JSON.stringify(respuestaFiltrado2)}`);
         return;
     }
 
-    console.log('FELICIDADES!! Todos los tests corrieron exitosamente');
+    console.log("\x1b[32m",'FELICIDADES!! Todos los tests corrieron exitosamente');
 })();
